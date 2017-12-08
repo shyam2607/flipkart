da694081aa5f56fa57a91a55d034252a10a7d001
shyam2607 <ramshyam26793@gmail.com>
2017-12-08 17:52:07 +0530

    updated

--- src/ecommerce/products.java
+++ src/ecommerce/products.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 public class  products 
 {
 	public static WebDriver driver;
-	String url= "https://www.flipkart.com/";
+	String url= "https://www.flipkart.commmmmmmmm/";
 	@BeforeTest
 	 	public void setup() throws InterruptedException
      { 
