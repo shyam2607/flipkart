7d85661b36285ec6e1fbe80c13a1d163d46b39a4
shyam2607 <34370679+shyam2607@users.noreply.github.com>
2017-12-08 17:56:27 +0530

    Update products.java

--- src/ecommerce/products.java
+++ src/ecommerce/products.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 public class  products 
 {
 	public static WebDriver driver;
-	String url= "https://www.flipkart.commmmmmmmm/";
+	String url= "https://www.flipkart.com/";
 	@BeforeTest
 	 	public void setup() throws InterruptedException
      { 
